5262ad04729c39096c1d4b9fcd787c8b5dcc939f
Dido Viktorov <d.viktorovchak@gmail.com>
2016-08-19 14:14:44 +0300

    Modifying Register and Login with username
    Change the action Login in AccountController - to use UserName instead
    of email.
    Change the action Register in AccountController - to use UserName
    instead of email, two additional fields added FullName and username.
    Changed Login and Register views.
    In ApplicationUser new property added - FullName for future uses.

--- Web Site/Models/AccountViewModels.cs
+++ Web Site/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace Web_Site.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -64,6 +63,14 @@ namespace Web_Site.Models
 
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+
         [Required]
         [EmailAddress]
         [Display(Name = "Email")]
