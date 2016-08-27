d0d437dcf8d5475d5d3cd9cdda0fc80a21b89252
karabinata <karabinata1992@gmail.com>
2016-08-27 04:24:52 +0300

    Comments
    Adding comments and categories, but the categories are not completely functuonal yet

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Web_Site.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public System.Data.Entity.DbSet<Web_Site.Models.Listings> Listings { get; set; }
+
+        public DbSet<File> Files { get; set; }
+
+        public DbSet<Comment> Comments { get; set; }
+
+        public System.Data.Entity.DbSet<Web_Site.Models.Categorie> Categories { get; set; }
+
+        // public System.Data.Entity.DbSet<Web_Site.Models.ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
