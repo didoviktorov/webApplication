405e7c1296cb4121b3496bdfb50ba7f8bbd133f0
h-stoyanov <icaknewton@gmail.com>
2016-08-29 10:35:17 +0300

    Changed Migration Config

--- Web Site/Migrations/Configuration.cs
+++ Web Site/Migrations/Configuration.cs
@@ -32,7 +32,9 @@ namespace Web_Site.Migrations
             //    );
             //
 
-            var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            // Add Admin Role to Roles Table
+           /* var RoleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
             string[] roleNames = { "Admin", "User" };
             IdentityResult roleResult;
             foreach (var roleName in roleNames)
@@ -42,9 +44,12 @@ namespace Web_Site.Migrations
                     roleResult = RoleManager.Create(new IdentityRole(roleName));
                 }
             }
+            */
 
-           // var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
-           // UserManager.AddToRole("4c08b733-28c0-4a64-af37-dd8368b47071", "Admin");
+            // Change From User to Admin Role by User Id
+            /* var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            UserManager.AddToRole("995d4a5b-6d23-45df-a4ee-0a1674a5aaf9", "Admin");
+            UserManager.RemoveFromRole("995d4a5b-6d23-45df-a4ee-0a1674a5aaf9", "User"); */
         }
     }
 }
