35e01f1a3baf582916c5b6ecaac43320a48bcc20
Dido Viktorov <d.viktorovchak@gmail.com>
2016-08-30 00:21:44 +0300

    Change home site view - changing pictures every few seceonds (6 sec)

--- Web Site/Scripts/openPhoto.js
+++ Web Site/Scripts/openPhoto.js
@@ -1,3 +1,18 @@
 ﻿ShowPopup = function (photoId) {
     window.open("/File?id=" + photoId, "PopupWindow", 'width=600px,height=400px,top=150,left=250');
 }
+
+function displayNextImage() {
+    x = (x === images.length - 1) ? 0 : x + 1;
+    document.getElementById("img").src = images[x];
+}
+
+function displayPreviousImage() {
+    x = (x <= 0) ? images.length - 1 : x - 1;
+    document.getElementById("img").src = images[x];
+}
+
+function startTimer() {
+}
+
+var images = [], x = -1;
